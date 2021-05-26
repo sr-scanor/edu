b4aa7713c98fbdfb7edd8ef3a1285fa7b846c1bf
sr-scanor <82767039+sr-scanor@users.noreply.github.com>
2021-05-25 22:02:57 -0300

    fff
    fwfw

---
+++ App.js
@@ -0,0 +1,22 @@
+
+import React from 'react';
+import {View,Text, StyleSheet} from 'react-native';
+import  {Navigation} from ' react-native-navbar ' ;
+
+export default function App1() {
+return (
+       <View >
+        <Text style={estilos.txt1}>Eduardo </Text>
+       
+       </View>
+);
+};
+
+const estilos = StyleSheet.create({
+  txt1:{
+    color:'#30600',
+
+  }
+
+
+})
